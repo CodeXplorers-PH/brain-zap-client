3088049a965426278e567b25e8286dab6381efc8
= <=>
2025-03-14 14:23:18 +0600

    Sticky scrool added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,31 @@ import Button from "../ui/Button";
 import { Turn as Hamburger } from "hamburger-react";
 import { useState } from "react";
 import { ChevronRight } from "lucide-react";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
 
 const Header = () => {
-  const [isOpen, setisOpen] = useState(false);
+  const { scrollY } = useScroll();
+  const [isHidden, setIsHidden] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsHidden(latest > scrollY.getPrevious());
+  });
 
   return (
     <>
-      {/* Header Section */}
-      <header className="flex-section">
-        <div className="wrapper py-5 gap-5 items-center justify-between">
+      <motion.header
+        initial={{ y: 0 }}
+        animate={{ y: isHidden ? "-100%" : "0%" }}
+        transition={{ duration: 0 }}
+        className="fixed top-0 left-0 flex justify-center w-full backdrop-blur-md z-[99999] transition-all"
+      >
+        <div className="wrapper py-3 gap-5 items-center justify-between">
           {/* Header Logo */}
           <Link to={`/`}>
             <img width={180} src={LogoURI} />
@@ -21,26 +37,24 @@ const Header = () => {
           <div className="flex items-center gap-14">
             {/* Header navs */}
             <nav
-              className={`w-full hidden top-20 p-5 fixed lg:flex  justify-center left-0 lg:static lg:w-fit ${
-                isOpen && "!flex"
-              }`}
+              className={`w-full hidden top-20 p-5 fixed lg:flex justify-center left-0 lg:static lg:w-fit`}
             >
-              <ul className="flex text-center bg-white gap-7 w-full flex-col border p-5 py-10 rounded-md">
+              <motion.ul className="flex justify-center text-center gap-5 bg-huf-purple/10 border-huf-purple/30 overflow-hidden lg:gap-7 w-full flex-col border p-5 py-10 rounded-xl lg:border-none lg:flex-row lg:p-0 lg:bg-transparent">
                 {Navs &&
                   Navs.map((navlink, index) => (
                     <li key={`navlink-${index}`}>
                       <Link
-                        className={`font-medium hover:text-text/80 transition-all text-text`}
+                        className="font-medium hover:text-text/80 transition-all text-text"
                         to={navlink.path}
                       >
                         {navlink.pathName}
                       </Link>
                     </li>
                   ))}
-                <div className="max-[520px]:hidden flex w-full justify-center">
+                <div className="min-[520px]:hidden flex w-full justify-center">
                   <HeaderButton />
                 </div>
-              </ul>
+              </motion.ul>
             </nav>
             <div className="flex items-center gap-5">
               <div className="max-[520px]:hidden flex items-center gap-5">
@@ -51,12 +65,19 @@ const Header = () => {
                   isOpen && "!bg-huf-purple/20 !border-huf-purple/50"
                 }`}
               >
-                <Hamburger size={25} toggled={isOpen} toggle={setisOpen} />
+                <Hamburger size={25} toggled={isOpen} toggle={setIsOpen} />
               </div>
             </div>
           </div>
         </div>
-      </header>
+      </motion.header>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div>
+            <h1>s</h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
@@ -71,7 +92,7 @@ export const HeaderButton = () => {
           Get Started{" "}
           <ChevronRight
             strokeWidth={1.5}
-            className={`absolute opacity-0 transition-all group-hover:translate-x-14 ml-2 group-hover:opacity-100`}
+            className="absolute opacity-0 transition-all group-hover:translate-x-14 ml-2 group-hover:opacity-100"
           />
         </Button>
       </Link>
