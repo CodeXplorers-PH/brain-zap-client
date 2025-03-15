a209eff42e1bb2d515bccc84f9313e08f5e08803
s-h-rumon <shahidhasanruman3@gmil.com>
2025-03-15 13:21:22 +0600

    Get Started cards designed

--- src/Pages/Home/Sections/GetStarted.jsx
+++ src/Pages/Home/Sections/GetStarted.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SectionHeading from '@/components/ui/SectionHeading';
 import { Link } from 'react-router-dom';
 import Button from '@/components/ui/Button';
+import { getStartData } from '@/data/GetStarted';
 
 const GetStarted = () => {
   return (
@@ -14,10 +15,36 @@ const GetStarted = () => {
         />
 
         {/* Get Started button */}
-        <div className="my-5 flex justify-center">
+        <div className="my-8 flex justify-center">
           <Link to="/pricing">
-            <Button>Get Started Now</Button>
+            <Button className="shadow-[0px_2px_4px_2px_#646464]">
+              Get Started Now
+            </Button>
           </Link>
+        </div>
+
+        {/* Cards */}
+        <div className="flex flex-wrap justify-center gap-5">
+          {getStartData.map((data, index) => (
+            <div
+              className={`w-full md:w-[calc(50%-10px)] lg:w-[calc(33.33%-13.33px)] p-8 md:p-9 rounded-3xl relative ${
+                index === 0
+                  ? 'bg-[#D2F2D2]'
+                  : index === 1
+                  ? 'bg-[#FFDF78]'
+                  : 'bg-[#AFDDFF]'
+              }`}
+              key={data.id}
+            >
+              <div
+                className={`bg-white/30 w-1/2 h-28 rounded-bl-[100%] absolute top-0 right-0`}
+              ></div>
+
+              <img className="w-24 aspect-square" src={data.image} alt="" />
+              <h5 className="text-xl font-bold my-5">{data.title}</h5>
+              <p>{data.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/data/GetStarted.js
+++ src/data/GetStarted.js
@@ -1,22 +1,22 @@
-export const getStarted = [
+export const getStartData = [
   {
     id: '01',
     title: 'Sharing advice, never your data',
-    image: '/knight.svg',
+    image: '/icons/knight.svg',
     description:
       "Your family's safety matters. Any information you provide us is secure and never shared with anyone, ever.",
   },
   {
     id: '02',
     title: 'Science-powered, AI-supported',
-    image: '/books.svg',
+    image: '/icons/books.svg',
     description:
       'Skip the 3am panic Googling. Riley uses the best medical data and pediatrician-endorsed resources to provide reliable insights in seconds.',
   },
   {
     id: '03',
     title: 'Riley grows with your family',
-    image: '/bunny.svg',
+    image: '/icons/bunny.svg',
     description:
       "There's no generic advice here! Riley personalizes itself to you based on the details you provide. The more details, the deeper the personalization.",
   },
