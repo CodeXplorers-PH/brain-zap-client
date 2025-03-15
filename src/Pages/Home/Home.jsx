fb762c8ed47b2a0357b263428ad37e962ac2317d
ornobaadi <aadi4789@gmail.com>
2025-03-15 16:19:01 +0600

    FAQ

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
+import FAQ from './Sections/FAQ';
 import Features from './Sections/Features';
 import Hero from './Sections/Hero';
-
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <FAQ />
     </>
   );
 };
