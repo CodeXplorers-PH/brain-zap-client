cc5251581423c15727afc4d8b5d0a5f68dbc6230
s-h-rumon <shahidhasanruman3@gmil.com>
2025-03-15 11:48:02 +0600

    GetStarted data added

--- src/Pages/Home/Sections/GetStarted.jsx
+++ src/Pages/Home/Sections/GetStarted.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SectionHeading from '@/components/ui/SectionHeading';
+import { Link } from 'react-router-dom';
+import Button from '@/components/ui/Button';
 
 const GetStarted = () => {
   return (
@@ -10,6 +12,13 @@ const GetStarted = () => {
           heading="How to Get Started"
           subHeading="Follow These Simple Steps to Start Creating Your AI-Powered Quizzes Today!"
         />
+
+        {/* Get Started button */}
+        <div className="my-5 flex justify-center">
+          <Link to="/pricing">
+            <Button>Get Started Now</Button>
+          </Link>
+        </div>
       </div>
     </section>
   );
