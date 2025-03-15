d915d85096a5ca630eff90c6483bbb335eca46c1
s-h-rumon <shahidhasanruman3@gmil.com>
2025-03-15 11:25:29 +0600

    Section Heading added to Get Started section

--- src/Pages/Home/Sections/GetStarted.jsx
+++ src/Pages/Home/Sections/GetStarted.jsx
@@ -3,8 +3,14 @@ import SectionHeading from '@/components/ui/SectionHeading';
 
 const GetStarted = () => {
   return (
-    <section className="flex-section bg-wheat py-20">
-      <div className="wrapper flex-col">{/* Heading */}</div>
+    <section className="flex-section bg-wheat pt-10 pb-20">
+      <div className="wrapper flex-col">
+        {/* Heading */}
+        <SectionHeading
+          heading="How to Get Started"
+          subHeading="Follow These Simple Steps to Start Creating Your AI-Powered Quizzes Today!"
+        />
+      </div>
     </section>
   );
 };
