ebb7c13cfa7379c23d2e36264f9aa5f99104a5c3
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-03-16 23:20:36 +0600

    [ADDED]: How it Works section added to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Features from "./Sections/Features";
 import Hero from "./Sections/Hero";
+import HowItWorks from "./Sections/HowItWorks/HowItWorks";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <HowItWorks />
     </>
   );
 };
