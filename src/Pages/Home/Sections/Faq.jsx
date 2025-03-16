6deab2ffb47c34623495d1b70bd6cb9adc20466d
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-03-16 16:30:34 +0600

    Faq responsive check done

--- src/Pages/Home/Sections/Faq.jsx
+++ src/Pages/Home/Sections/Faq.jsx
@@ -18,23 +18,24 @@ const Faq = () => {
       />
       <div>
         {/* FAQ animation */}
-        <div className="absolute right-20 top-37">
+        <div className="absolute lg:right-20 right-0 lg:top-37 top-45">
           <Lottie
             style={{ width: 100, height: 100 }}
             animationData={animationData}
             loop={true}
           />
         </div>
+        {/* Accordion */}
         <Accordion
           type="single"
           collapsible
-          className="w-[80%] lg:w-3/4 mx-auto bg-white p-8 mt-12 rounded-xl"
+          className="w-[80%] lg:w-3/4 mx-auto bg-white p-8 mt-12 rounded-xl "
         >
-          <AccordionItem value="item-1 ">
-            <AccordionTrigger className="text-xl">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="lg:text-xl text-base text-text">
               How do I know I can trust BrainZap?
             </AccordionTrigger>
-            <AccordionContent className="list-disc text-lg">
+            <AccordionContent className="list-disc lg:text-lg text-sm text-text">
               <li>
                 Reliable AI Technology – Our AI generates quizzes based on
                 verified sources to ensure accuracy and engagement.
@@ -51,10 +52,10 @@ const Faq = () => {
           </AccordionItem>
 
           <AccordionItem value="item-2">
-            <AccordionTrigger className="text-xl">
+            <AccordionTrigger className="lg:text-xl text-base text-text">
               How will you keep my data private?
             </AccordionTrigger>
-            <AccordionContent className="text-lg">
+            <AccordionContent className="lg:text-lg text-sm text-text">
               This platform can ensure you that, your data is fully secure with
               the organization. Only you can access your data. After successful
               login we verify the users credentials with the cookies.
@@ -62,20 +63,20 @@ const Faq = () => {
           </AccordionItem>
 
           <AccordionItem value="item-3">
-            <AccordionTrigger className="text-xl">
+            <AccordionTrigger className="lg:text-xl tex-base text-text">
               Do you have a free trial?
             </AccordionTrigger>
-            <AccordionContent className="text-lg">
+            <AccordionContent className="lg:text-lg text-sm text-text">
               Yes, we do have a free trial in timed mode of our quiz. A logged
               in user can play a demo exam for the first time to test himself.
             </AccordionContent>
           </AccordionItem>
 
           <AccordionItem value="item-4">
-            <AccordionTrigger className="text-xl">
+            <AccordionTrigger className="lg:text-xl text-base text-text">
               What kind of people can use this app?
             </AccordionTrigger>
-            <AccordionContent className="text-lg">
+            <AccordionContent className="lg:text-lg text-sm text-text">
               <li>Students can prepare for a programming related challenge.</li>
               <li>Self learners can improve their coding skill.</li>
               <li>Professionals can seek deep knowledge about AI.</li>
@@ -86,10 +87,10 @@ const Faq = () => {
           </AccordionItem>
 
           <AccordionItem value="item-5">
-            <AccordionTrigger className="text-xl">
+            <AccordionTrigger className="lg:text-xl text-base text-text">
               Do we have the premium version of BrainZap?
             </AccordionTrigger>
-            <AccordionContent className="text-lg">
+            <AccordionContent className="lg:text-lg text-sm text-text">
               Yes, for all the users we have some premium quizzes for better
               user experience.
             </AccordionContent>
