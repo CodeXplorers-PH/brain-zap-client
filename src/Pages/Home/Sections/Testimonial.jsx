b4ef7df55f42259d3cce164d25b530e6da1f786e
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-03-16 22:56:45 +0600

    testimonial cards responsive check done

--- src/Pages/Home/Sections/Testimonial.jsx
+++ src/Pages/Home/Sections/Testimonial.jsx
@@ -10,6 +10,7 @@ const Testimonial = () => {
         heading="Testimonial"
         subHeading="BrainZap is transforming the way people learn with AI-powered quizzes! From students to professionals, our users love how it makes learning engaging. See what they have to say!"
       />
+      {/* Testimonial Cards */}
       <div className="w-11/12 mx-auto mt-12 lg:p-12 p-6 rounded-xl bg-gradient-to-r from-[#ff93ab] to-[#fdb154] grid lg:grid-cols-3 grid-cols-1 gap-6">
         {testimonialData.map((testimonialData) => (
           <TestimonialCard
