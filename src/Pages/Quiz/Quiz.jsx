7628a70a705926a3bb5d0d2274d51906c824636b
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-03-24 00:40:20 +0600

    [Added]: Quiz Answer page implemented with right and wrong ans

--- src/Pages/Quiz/Quiz.jsx
+++ src/Pages/Quiz/Quiz.jsx
@@ -1,9 +1,13 @@
 import Button from "@/components/ui/Button";
 import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Quiz = ({ questions }) => {
   const [selectedOptions, setSelectedOptions] = useState({});
+  const { category } = useParams(); // Using category from URL params
+  const navigate = useNavigate();
 
+  //   User selected options
   const handleOptionSelect = (questionIndex, option) => {
     setSelectedOptions({
       ...selectedOptions,
@@ -11,7 +15,16 @@ const Quiz = ({ questions }) => {
     });
   };
 
-  const optionLabels = ["A.", "B.", "C.", "D."]; // Labels for options
+  // Labels for options
+  const optionLabels = ["A.", "B.", "C.", "D."];
+
+  const handleSubmit = () => {
+    // Save selected answers in localStorage
+    localStorage.setItem("userAnswers", JSON.stringify(selectedOptions));
+    console.log(selectedOptions);
+    // Redirect to the result page using navigate
+    navigate(`/quiz/${category}/answer`);
+  };
 
   return (
     <div className="space-y-6 p-4 container mx-auto">
@@ -25,9 +38,8 @@ const Quiz = ({ questions }) => {
             key={index}
             className="bg-white/30 shadow-xl rounded-lg overflow-hidden p-6 hover:shadow-lg transition-shadow duration-300"
           >
-            <h3 className="text-xl font-semibold  mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-500 to-purple-800 ">
-              {`Question ${index + 1}: ${q.question}`}{" "}
-              {/* Question numbering */}
+            <h3 className="text-xl font-semibold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-500 to-purple-800">
+              {`Question ${index + 1}: ${q.question}`}
             </h3>
             <div className="grid grid-cols-2 gap-4">
               {q.options.map((option, i) => (
@@ -49,9 +61,10 @@ const Quiz = ({ questions }) => {
         ))
       )}
       <div className="w-full">
-        <Button className={"w-full"}>Submit</Button>
+        <button className="w-full px-10 py-[7px] flex focus:ring-2 ring-offset-2 focus:ring-huf-purple justify-center group items-center gap-2 rounded-full bg-gradient-to-r hover:bg-purple-950 transition-all from-huf-purple via-huf-purple/70 to-huf-purple/80 border border-huf-purple text-white" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
-      <div></div>
     </div>
   );
 };
