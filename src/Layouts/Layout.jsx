ad18a702637217fc330b49a3e1f89399df5d7dfd
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-04-14 04:23:32 +0600

    [Updated]: chat system connected to backend ai model and working properly

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -4,13 +4,15 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { Outlet } from "react-router-dom";
 import ZapChatWidget from "@/components/ZapChatWidget/ZapChatWidget";
+import useAuth from "@/hooks/useAuth";
 
 const Layout = () => {
+  const { user } = useAuth();
   return (
     <>
       {/* Header */}
       <Header />
-      <ZapChatWidget userType="elite"></ZapChatWidget>
+      {user && <ZapChatWidget />}
       <Outlet />
       <Footer />
     </>
