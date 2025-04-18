e6bad430d18ef5c5a76f7145df5ea61c11bcecb1
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-04-18 18:31:23 +0600

    commit before merge

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -2,17 +2,26 @@
 
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ZapChatWidget from "@/components/ZapChatWidget/ZapChatWidget";
 import useAuth from "@/hooks/useAuth";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
   const { user } = useAuth();
+  const location = useLocation();
+  const [chat, setChat] = useState(true);
+  console.log(location?.pathname);
+
+  useEffect(() => {
+    const isQuizPage = location.pathname.startsWith("/quiz");
+    setChat(!isQuizPage);
+  }, [location?.pathname]);
   return (
     <>
       {/* Header */}
       <Header />
-      {user && <ZapChatWidget />}
+      {user && chat && <ZapChatWidget />}
       <Outlet />
       <Footer />
     </>
