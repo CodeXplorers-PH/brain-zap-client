601a6df6b6509473704472ec43206b071c0c2892
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-05-02 02:07:57 +0600

    added dynamic title for home and quiz page

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
 import PricingPlan from "./Sections/PricingPlan";
 import FreeTrial from "./Sections/FreeTrial";
+import { Helmet } from "react-helmet";
 
 const Pricing = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="bg-gray-900 min-h-screen">
-      <PricingPlan />
-      <FreeTrial />
-    </div>
+    <>
+      <Helmet>
+        <title>Brain Zap AI | Pricing</title>
+      </Helmet>
+      <div className="bg-gray-900 min-h-screen">
+        <PricingPlan />
+        <FreeTrial />
+      </div>
+    </>
   );
 };
 
