d25b6e1745d53f669718249faee39ad485e0e40a
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-05-01 22:00:48 +0600

    fixing merging error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -194,7 +194,6 @@ const Header = () => {
             ease: "easeInOut",
             delay: isOpen ? 0.05 : 0,
           }}
-          className="lg:hidden flex flex-col absolute -top-[50px] left-[112px] -translate-1/2 text-left space-y-4"
           className="lg:hidden flex flex-col font-semibold absolute -top-[50px] left-[112px] -translate-1/2 text-left space-y-4"
         >
           {Navs.map((navlink, index) => (
