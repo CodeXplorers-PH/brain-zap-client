476f88b43f093348ff9dcb644325cd1bff092002
s-h-rumon <shahidhasanruman3@gmil.com>
2025-05-02 12:19:31 +0600

    Updated: getStreak on quiz answer

--- src/hooks/useStreak.js
+++ src/hooks/useStreak.js
@@ -1,57 +1,71 @@
 import { useEffect, useState } from 'react';
 import useAxiosSecure from './useAxiosSecure';
 import useAuth from './useAuth';
+import { useLocation } from 'react-router-dom';
 
 const useStreak = () => {
   const { user } = useAuth();
   const [streak, setStreak] = useState(0);
   const axiosSecure = useAxiosSecure();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    (async () => {
-      if (!user?.email) {
-        setStreak(0);
-        return;
-      }
+    if (!user?.email) {
+      setStreak(0);
+      return;
+    }
 
-      const { data: userQuizzesDate } = await axiosSecure.get(
-        '/quizzes_streak_date'
-      );
-
-      const today = new Date();
-      const todayStr = today.toLocaleDateString('en-CA');
-
-      const formatDateLocal = dateStr => {
-        const date = new Date(dateStr);
-        return date.toLocaleDateString('en-CA');
-      };
-
-      const quizDaysSet = new Set(
-        userQuizzesDate.map(({ date }) => formatDateLocal(date))
-      );
-
-      if (!quizDaysSet.has(todayStr)) {
-        setStreak(0);
-        return;
-      }
-
-      let streakCount = 1;
-
-      for (let i = 1; ; i++) {
-        const prevDate = new Date();
-        prevDate.setDate(today.getDate() - i);
-        const prevStr = prevDate.toLocaleDateString('en-CA');
-
-        if (quizDaysSet.has(prevStr)) {
-          streakCount++;
-        } else {
-          break;
-        }
-      }
-
-      setStreak(streakCount);
-    })();
+    setStreak(getStreak());
   }, [user]);
+
+  useEffect(() => {
+    if (!user?.email) {
+      setStreak(0);
+      return;
+    }
+
+    if (pathname.includes('quiz') && pathname.includes('answer')) {
+      setStreak(getStreak());
+    }
+  }, [pathname]);
+
+  const getStreak = async () => {
+    const { data: userQuizzesDate } = await axiosSecure.get(
+      '/quizzes_streak_date'
+    );
+
+    const today = new Date();
+    const todayStr = today.toLocaleDateString('en-CA');
+
+    const formatDateLocal = dateStr => {
+      const date = new Date(dateStr);
+      return date.toLocaleDateString('en-CA');
+    };
+
+    const quizDaysSet = new Set(
+      userQuizzesDate.map(({ date }) => formatDateLocal(date))
+    );
+
+    if (!quizDaysSet.has(todayStr)) {
+      return 0;
+    }
+
+    let streakCount = 1;
+
+    for (let i = 1; ; i++) {
+      const prevDate = new Date();
+      prevDate.setDate(today.getDate() - i);
+      const prevStr = prevDate.toLocaleDateString('en-CA');
+
+      if (quizDaysSet.has(prevStr)) {
+        streakCount++;
+      } else {
+        break;
+      }
+    }
+
+    return streakCount;
+  };
 
   return streak;
 };
