fefa9adfe1ffabf4739f0cf8d5e2038c74b2c444
s-h-rumon <shahidhasanruman3@gmil.com>
2025-05-03 14:43:20 +0600

    Added: tanstackQuery added to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/Router"; // React Router Routes
-import "./style.css"; // Style
-import AuthProvider from "./provider/AuthProvider";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Router'; // React Router Routes
+import './style.css'; // Style
+import AuthProvider from './provider/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById("root")).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} /> {/* Router Provider */}
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} /> {/* Router Provider */}
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
 );
